d1d4854b6a23dd23ec23ede81574dc7ba71cff85
GithiuIsaac <Isaacndarwa@gmail.com>
2021-05-31 03:54:11 +0300

    Updated postgresql string

--- pharmtool/config.py
+++ pharmtool/config.py
@@ -1,10 +1,15 @@
 import os
+import re
 
 class Config():
     # Secret key (token) to prevent cookie modification & X-site request forgery attacks
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # Pass these as environment variables to prevent unauthroized access
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
 
     # SQLite - sqlite:///db.db - relative path, DB is located in the project dir
     
